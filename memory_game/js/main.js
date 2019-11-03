521838f48115d5713d5e1e7740f2f5d1a4c33c9f
Tara Young <ty18881@gmail.com>
2019-11-02 20:44:46 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,32 @@
 console.log("up and running");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour ="king";
 
-console.log("User flipped " + cardFour);
-console.log("User flipped " + cardTwo);
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+
+cardsInPlay.push(cardOne);
+
+// console.log("user flipped "+ cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+// console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1])
+	{
+		alert("You found a match");
+	}
+	else
+	{
+		alert("Sorry. Try again");
+	}
+	
+}
