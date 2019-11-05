31d6fc7b3dfd0133fdb8b5627aead5209fc23670
Tara Young <ty18881@gmail.com>
2019-11-05 06:04:56 -0500

    added card objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,28 @@ console.log("up and running");
 
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
@@ -21,8 +42,11 @@ if (cardsInPlay[0] === cardsInPlay[1])
 
 function flipCard(cardId){
 
-console.log("User flipped "+ cards[cardId])
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped "+ cards[cardId].rank);
+console.log("Card Image "+ cards[cardId].cardImage);
+console.log("Card Rank "+ cards[cardId].rank);
+
+cardsInPlay.push(cards[cardId].rank);
 
 checkForMatch();
 }
